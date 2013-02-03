24613a8bfb9306285af62eb483f0c53b5732cd0f
Felix GV <felix@sharplinker.com>
2013-02-02 19:29:32 -0500

    Fixed the Play 2 Controller fileTemplate since it had the import before the package...

--- fileTemplates/Play 2 Controller.scala
+++ fileTemplates/Play 2 Controller.scala
@@ -1,6 +1,7 @@
+#if ((${PACKAGE_NAME} && ${PACKAGE_NAME} != ""))package ${PACKAGE_NAME} #end
+
 import play.api.mvc.{Action, Controller}
 
-#if ((${PACKAGE_NAME} && ${PACKAGE_NAME} != ""))package ${PACKAGE_NAME} #end
 #parse("File Header.java")
 object ${NAME} extends Controller {
 }
